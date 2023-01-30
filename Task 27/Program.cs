782a0955ac19542342f91054ab570631def75f34
Andrey <chizhov67@gmail.com>
2023-01-30 20:10:50 +0300

    Task27

---
+++ Task 27/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleAppTask27
+{
+	internal class Program
+	{
+		static int sumNumbers(int N)
+		{
+			int ost, sum = 0;
+			while (N != 0)
+			{
+				ost = N % 10;
+				N = N / 10;
+				sum += ost;
+			}
+			
+			return sum;
+		}
+        
+
+		static void Main(string[] args)
+		{
+			int N;
+
+			Console.Write("Введите число: ");
+
+			while(!int.TryParse(Console.ReadLine(), out N))
+			{
+				Console.WriteLine("\nНеверный ввод, введите число");
+				Console.Write("\nВведите число: ");
+			}
+
+			Console.WriteLine("\nСумма цифр числа " + N + " = " + sumNumbers(N));
+
+			Console.ReadLine();
+		}
+	}
+}
+
